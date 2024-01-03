5ce5c7478a04bc18fb91bb94cb2860849c05bc68
chidismith01 <emereuwachidiebere94@gmail.com>
2024-01-03 10:10:41 -0800

    python

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,17 +8,17 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *step2, *step1;
+	listint_t *fast, *slow;
 	if (!list || !list->next)
 		return(0);
-	step2 = list;
-	step1 = list;
+	fast = list;
+	slow  = list;
 
-	while(step1 != NULL && step2 != NULL && step2->next != NULL)
+	while(slow != NULL && fast != NULL && fast->next != NULL)
 	{
-		step1 = step1->next;
-		step2 = step2->next;
-		if(step1 == step2)
+		slow = slow->next;
+		fast = fast->next->next;
+		if(slow == fast)
 		{
 			return (1);
 		}
