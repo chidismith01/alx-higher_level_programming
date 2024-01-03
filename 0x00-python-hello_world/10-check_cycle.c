436d021e302448efe5a136f02ac4fd77763f0f92
chidismith01 <emereuwachidiebere94@gmail.com>
2024-01-03 10:03:00 -0800

    python

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,1 +1,27 @@
-#include "list.h'
+#include "lists.h"
+
+/**
+ * check_cycle - function that checks if a list has a cycle
+ * @list: linked list
+ * return: return 1 if there is cycle else 0
+**/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *step2, *step1;
+	if (!list || !list->next)
+		return(0);
+	step2 = list;
+	step1 = list;
+
+	while(step1 != NULL && step2 != NULL && step2->next != NULL)
+	{
+		step1 = step1->next;
+		step2 = step2->next;
+		if(step1 == step2)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
--- 0x00-python-hello_world/10-linked_lists.c
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -5,56 +5,59 @@
 /**
  * print_listint - prints all elements of a listint_t list
  * @h: pointer to head of list
- * return: numbers of nodes
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *current;
-	unsigned int n; /* number of nodes*/
+    const listint_t *current;
+    unsigned int n; /* number of nodes */
 
-	current = h;
-	n = 0;
-	while(current != NULL)
-	{
-		print("%i\n",curent->n);
-		current = current->next
-		n++;
-	}
-	return(n)
+    current = h;
+    n = 0;
+    while (current != NULL)
+    {
+        printf("%i\n", current->n);
+        current = current->next;
+        n++;
+    }
+
+    return (n);
 }
+
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list
  * @head: pointer to a pointer of the start of the list
  * @n: integer to be included in node
- * return: address of the new element or NULL if it fails
+ * Return: address of the new element or NULL if it fails
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new;
-	
-	new = malloc(sizeof(listint_t));
-	if(new == NULL) /* condition for our new node if error occurs */
-		return (NULL);
+    listint_t *new;
 
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	
-	return (new);
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = n;
+    new->next = *head;
+    *head = new;
+
+    return (new);
 }
 
 /**
- * free_listint -free a listint_t list
- * @head: pointer to list to be free
- * return: viod
+ * free_listint - frees a listint_t list
+ * @head: pointer to list to be freed
+ * Return: void
  */
 void free_listint(listint_t *head)
 {
-	listint_t *current;
-	
-	while (head != NULL)
-		head = head->next;
-	free(current);
+    listint_t *current;
 
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current);
+    }
 }
-
