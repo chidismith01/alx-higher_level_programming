26580dd6bc6ae781768d37e65224e130a1fed4f1
chidismith01 <emereuwachidiebere94@gmail.com>
2024-01-07 10:42:46 -0800

    This program that prints number from 0 to 98 - numbers must be seperated by , followed by space

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for numbers in range(0, 100):
+for numbers in range(0, 99):
     print('{:02d}'.format(numbers), end=', ')
-    print('{:02d}'.format(numbers + 1))
+print('{:02d}'.format(numbers + 1))
