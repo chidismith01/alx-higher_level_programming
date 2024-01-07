5367889c97ec941ffb41887ed724673fa39977be
chidismith01 <emereuwachidiebere94@gmail.com>
2024-01-07 05:23:33 -0800

    A  program that prints all numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for numbers in range(0, 100):
-    print('{:02d}, '.format(numbers), end=',')
+    print('{:02d}, '.format(numbers), end=', ')
     print('{:02d}'.format(number + 1))
