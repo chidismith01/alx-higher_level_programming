2415fe7c65d31daef2c54c9f541a57403e88ca59
chidismith01 <emereuwachidiebere94@gmail.com>
2024-01-16 10:55:59 -0800

    A program that imports the variable a from the file variable_load_5.py and prints its value.

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+
+    num_args = len(sys.argv) - 1
+    if num_args != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    operator = sys.argv[2]
+    op = operator
+    if op != '+' and op != '-' and op != '*' and op != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    from calculator_1 import add, sub, mul, div
+    a = 0
+    for i in range(len(sys.argv) - 1):
+            a = int(sys.argv[i + 1])
+            b = int(sys.argv[i + 1])
+
+            if op == '+':
+                print("{} + {} = {}".format(a, b, add(a, b)))
+            elif op == '-':
+                print("{} - {} = {}".format(a, b, sub(a, b)))
+            elif op == '*':
+                print("{} * {} = {}".format(a, b, mul(a, b)))
+            else:
+                print("{} / {} = {}".format(a, b, div(a, b)))
