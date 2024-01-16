433dec069887e8bce958ac83b2f0420563602f61
chidismith01 <emereuwachidiebere94@gmail.com>
2024-01-16 01:10:24 -0800

    the Python function def magic_calculation(a, b, c): that does exactly the same as the following Python bytecode:

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''the Python function def magic_calculation(a, b, c): that takes three arguments and does exactly the same as python bytecode:'''
+def magic_calculation(a, b, c):
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
