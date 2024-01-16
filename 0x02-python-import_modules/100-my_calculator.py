7d4778390c363a0ad6d95073974a9a6f885436f5
chidismith01 <emereuwachidiebere94@gmail.com>
2024-01-16 11:00:16 -0800

    A program that imports the variable a from the file variable_load_5.py and prints its value.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -15,16 +15,13 @@ if __name__ == "__main__":
         sys.exit(1)
 
     from calculator_1 import add, sub, mul, div
-    a = 0
-    for i in range(len(sys.argv) - 1):
-            a = int(sys.argv[i + 1])
-            b = int(sys.argv[i + 1])
-
-            if op == '+':
-                print("{} + {} = {}".format(a, b, add(a, b)))
-            elif op == '-':
-                print("{} - {} = {}".format(a, b, sub(a, b)))
-            elif op == '*':
-                print("{} * {} = {}".format(a, b, mul(a, b)))
-            else:
-                print("{} / {} = {}".format(a, b, div(a, b)))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if op == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
