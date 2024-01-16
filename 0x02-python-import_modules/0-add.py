8c286e0dff8743aae1a38ac726bb3f01404ee267
chidismith01 <emereuwachidiebere94@gmail.com>
2024-01-16 01:23:03 -0800

    a program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if _name_ == "_main__":
+    '''print the sum of 1 & 2'''
+    from add_0 import add
+    a = 1
+    b = 2
+    print('{} + {} = {}'.format(a,b, add(a, b)))
+    
