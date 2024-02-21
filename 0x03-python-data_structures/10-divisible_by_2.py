fca4247d1d46c46f43abdfb624d69361edb1f2e1
chidismith01 <emereuwachidiebere94@gmail.com>
2024-02-21 14:52:37 -0800

    a function that finds all multiples of 2 in a list

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module contains a function to check if
+elements in a list are divisible by 2.
+"""
+
+
+def divisible_by_2(my_list=[]):
+    """
+    check if elements in a list are divisible by 2
+
+    Args:
+        my_list(list)_: The list to check.
+    
+    Return:
+    list: A list of booleans indicating if each element is divisible by 2
+    """
+    mul = []
+    for _. num in enumerate(my_list):
+        if num % 2 == 0:
+            mul.append(True)
+        else:
+            mul.append(False)
+
+    return mul
